47f2083c135151e8690d7b4ccd2f16f24e05f788
ayeniayodeji2009 <ayeniayodeji2009@gmail.com>
2023-07-02 13:40:49 +0100

    Redesign the Home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,19 +20,23 @@ function Home() {
         />
         </Helmet>
             <Navigation />
-            <h1>Hello Scissor Home</h1>
-            <p>I am the landing page</p>
-            <p>Scissor is a URL shortener that allows you to shorten your long links.</p>
-            <h1>Hello Scissor Home</h1>
-            <p>I am the landing page</p>
-            <p>Scissor is a URL shortener that allows you to shorten your long links.</p>
-            <h1>Hello Scissor Home</h1>
-            <p>I am the landing page</p>
-            <p>Scissor is a URL shortener that allows you to shorten your long links.</p>
-            <h1>Hello Scissor Home</h1>
-            <p>I am the landing page</p>
-            <p>Scissor is a URL shortener that allows you to shorten your long links.</p>
+            <section className="home__hero">
+                <div className="home__heroText_heading">
+                    <h1>Optimize Your Online Experience with Our <br /> 
+                    Advanced <span className="blue_heading">URL Shortening</span> Solution</h1>
+                </div>
+                <div className="home__heroText_paragragh">
+                    <p>Personalize your shortened URLs to align with your brand identity. Utility custom slugs, <br />
+                       branded links, and domain customization options to reinforce your brand presence and <br />
+                        enhance user engagement.</p>
+                </div>
 
+               <div className="home__heroBtns">
+                    <a href="/register" className="home__heroBtns_btn">Sign Up</a>
+                    <a href="/about_us" className="">Learn more</a>
+                </div>
+            </section>
+            <img src="../scissor_homepage_image_1.png" alt="hero" className="home__heroImg" />
         </div>
     )
 }
